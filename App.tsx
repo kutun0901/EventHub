34789eccef7cf918f152e8fc20f68c7be9a9ba70
kutun0901 <tuan.tq2433@gmail.com>
2024-04-30 08:41:16 -0700

    setup splash time window

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  // Setup how long to display Splash screen
+  const [isShowSplash, setIsShowPlash] = useState(true);
+
+    // Setup how long to display Splash screen
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setIsShowPlash(false)
+    }, 1500);
+
+    return () => clearTimeout(timeOut)
+  })
+
   return (
     <View>
       <Text>App</Text>
