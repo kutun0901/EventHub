09b21cba691d68b78d235e9f0a730be79fa96f48
kutun0901 <tuan.tq2433@gmail.com>
2024-05-08 14:55:22 -0700

    designing container component

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -4,32 +4,29 @@ import { ButtonComponent, InputComponent } from '../../components'
 import { globalStyles } from '../../styles/globalStyles'
 import { Lock, Sms } from 'iconsax-react-native'
 import { appColors } from '../../constants/appColors'
+import ContainerComponent from '../../components/ContainerComponent'
 
 const LoginScreen = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   return (
-    <View style={[globalStyles.container, {
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20
-    }]}>
+    <ContainerComponent>
       <InputComponent value={email}
-      onChange={val => setEmail(val)}
-      placeHolder='Email'
-      allowClear
-      prefix={<Sms size={22} color={appColors.gray} />}
+        onChange={val => setEmail(val)}
+        placeHolder='Email'
+        allowClear
+        prefix={<Sms size={22} color={appColors.gray} />}
       />
 
       <InputComponent value={password}
-      onChange={val => setPassword(val)}
-      placeHolder='Password'
-      isPassword
-      allowClear
-      prefix={<Lock size={22} color={appColors.gray} />}
+        onChange={val => setPassword(val)}
+        placeHolder='Password'
+        isPassword
+        allowClear
+        prefix={<Lock size={22} color={appColors.gray} />}
       />
-    </View>
+    </ContainerComponent>
   )
 }
 
