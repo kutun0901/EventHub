376ca34485dc710596a6427e838db3f16d481014
kutun0901 <tuan.tq2433@gmail.com>
2024-06-06 09:21:49 -0700

    exploreNAvigator

---
+++ src/navigators/ExplorerNavigator.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from '../screens';
+
+const ExplorerNavigator = () => {
+    const Stack = createNativeStackNavigator();
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    );
+}
+
+export default ExplorerNavigator
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,10 +1,15 @@
+import AddNewScreen from "./AddNewScreen";
 import SplashScreen from "./SplashScreen";
 import ForgotPassword from "./auth/ForgotPassword";
 import LoginScreen from "./auth/LoginScreen";
 import SignupScreen from "./auth/SignupScreen";
 import Verification from "./auth/Verification";
+import EventsScreen from "./events/EventsScreen";
 import HomeScreen from "./home/HomeScreen";
+import MapScreen from "./map/MapScreen";
+import ProfileScreen from "./profile/ProfileScreen";
 
 export {
-    HomeScreen, LoginScreen, SignupScreen, SplashScreen, ForgotPassword, Verification
+    HomeScreen, LoginScreen, SignupScreen, SplashScreen, ForgotPassword, Verification,
+    AddNewScreen, ProfileScreen, MapScreen, EventsScreen
 }
