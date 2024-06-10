0ed992fd4c0df6f929f286fa350acb6c506a63f3
kutun0901 <tuan.tq2433@gmail.com>
2024-06-10 10:26:47 -0700

    homeScreen in progress top tab

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,9 +1,14 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TouchableOpacity, StatusBar, Platform } from 'react-native'
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { authSelector, removeAuth } from '../../redux/reducers/authReducer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {GoogleSignin} from '@react-native-google-signin/google-signin'
+import { CircleComponent, RowComponent, TextComponent } from '../../components';
+import { HambergerMenu, Notification } from 'iconsax-react-native';
+import { appColors } from '../../constants/appColors';
+import { fontFamily } from '../../constants/fontFamily';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -11,16 +16,70 @@ const HomeScreen = () => {
   const auth = useSelector(authSelector);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Logout"
-        onPress={async () => {
-          await AsyncStorage.clear();
-          await GoogleSignin.signOut()
-          dispatch(removeAuth({}));
-        }}
-      />
+    <View style={[globalStyles.container]}>
+      <StatusBar barStyle={'light-content'} />
+
+      <View
+        style={{
+          backgroundColor: appColors.primary,
+          height: 179,
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40,
+          paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 52,
+          paddingHorizontal: 16,
+        }}>
+        <RowComponent>
+          <TouchableOpacity>
+            <HambergerMenu size={24} color={appColors.white} />
+          </TouchableOpacity>
+          <View style={[{flex: 1, alignItems: 'center'}]}>
+            <RowComponent>
+              <TextComponent
+                text="Current Location"
+                color={appColors.white2}
+                size={12}
+              />
+              <MaterialIcons
+                name="arrow-drop-down"
+                size={18}
+                color={appColors.white}
+              />
+            </RowComponent>
+            <TextComponent
+              text="New York, USA"
+              flex={0}
+              color={appColors.white}
+              font={fontFamily.medium}
+              size={13}
+            />
+          </View>
+
+          <CircleComponent color="#524CE0" size={36}>
+            <View>
+              <Notification size={18} color={appColors.white} />
+              <View
+                style={{
+                  backgroundColor: '#02E9FE',
+                  width: 10,
+                  height: 10,
+                  borderRadius: 4,
+                  borderWidth: 2,
+                  borderColor: '#524CE0',
+                  position: 'absolute',
+                  top: -2,
+                  right: -2,
+                }}
+              />
+            </View>
+          </CircleComponent>
+        </RowComponent>
+      </View>
+      <View
+        style={[
+          {
+            flex: 1,
+          },
+        ]}></View>
     </View>
   );
 };
