8cab54e4df4bc473019676e6abf1d1ac1a3ff63a
kutun0901 <tuan.tq2433@gmail.com>
2024-07-17 14:33:40 -0700

    drop down picker and modalize library to show bottom up modal

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TabNavigator from './TabNavigator'
 import DrawerNavigation from './DrawerNavigation'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const MainNavigator = () => {
 
     const Stack = createNativeStackNavigator()
-    return <Stack.Navigator screenOptions={{
-        headerShown: false
-    }}>
-        <Stack.Screen name='Main' component={DrawerNavigation} />
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
 
-    </Stack.Navigator>
+            <Stack.Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name='Main' component={DrawerNavigation} />
+
+            </Stack.Navigator>
+        </GestureHandlerRootView>
+    )
 }
 
 export default MainNavigator
