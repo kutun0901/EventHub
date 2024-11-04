cfd8b8591c91380423c1cc158c0da5eac57d224a
kutun0901 <tuan.tq2433@gmail.com>
2024-11-04 10:11:00 -0800

    modify event item to display on map

--- src/components/EventItem.tsx
+++ src/components/EventItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import TextComponent from './TextComponent';
 import CardComponent from './CardComponent';
@@ -13,6 +13,7 @@ import { fontFamily } from '../constants/fontFamily';
 import { globalStyles } from '../styles/globalStyles';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {useNavigation} from '@react-navigation/native';
+import { DateTime } from '../utils/DateTime';
 
 
 interface Props {
@@ -27,61 +28,103 @@ const EventItem = (props: Props) => {
 
     return (
         <CardComponent
-            isShadow
-            styles={{ width: appInfo.sizes.WIDTH * 0.7 }}
-            onPress={() => navigation.navigate('EventDetail', {item})}>
-            <ImageBackground
-                style={{ flex: 1, marginBottom: 12, height: 131, padding: 10 }}
-                source={require('../assets/images/event-image.png')}
-                imageStyle={{
-                    resizeMode: 'cover',
-                    borderRadius: 12,
-                }}>
-                <RowComponent justify="space-between">
-                    <CardComponent styles={[globalStyles.noSpaceCard]} color="#ffffffB3">
-                        <TextComponent
-                            color={appColors.danger2}
-                            font={fontFamily.bold}
-                            size={18}
-                            text="10"
-                        />
-                        <TextComponent
-                            color={appColors.danger2}
-                            font={fontFamily.semiBold}
-                            size={10}
-                            text="JUNE"
-                        />
-                    </CardComponent>
-                    <CardComponent styles={[globalStyles.noSpaceCard]} color="#ffffffB3">
-                        <MaterialIcons
-                            name="bookmark"
-                            color={appColors.danger2}
-                            size={22}
-                        />
-                    </CardComponent>
-                </RowComponent>
-
-            </ImageBackground>
+      isShadow
+      styles={{width: appInfo.sizes.WIDTH * 0.7}}
+      onPress={() => navigation.navigate('EventDetail', {item})}>
+      {type === 'card' ? (
+        <>
+          <ImageBackground
+            style={{flex: 1, marginBottom: 12, height: 131, padding: 10}}
+            source={require('../assets/images/event-image.png')}
+            imageStyle={{
+              resizeMode: 'cover',
+              borderRadius: 12,
+            }}>
+            <RowComponent justify="space-between">
+              <CardComponent
+                styles={[globalStyles.noSpaceCard]}
+                color="#ffffffB3">
+                <TextComponent
+                  color={appColors.danger2}
+                  font={fontFamily.bold}
+                  size={18}
+                  text="10"
+                />
+                <TextComponent
+                  color={appColors.danger2}
+                  font={fontFamily.semiBold}
+                  size={10}
+                  text="JUNE"
+                />
+              </CardComponent>
+              <CardComponent
+                styles={[globalStyles.noSpaceCard]}
+                color="#ffffffB3">
+                <MaterialIcons
+                  name="bookmark"
+                  color={appColors.danger2}
+                  size={22}
+                />
+              </CardComponent>
+            </RowComponent>
+          </ImageBackground>
+          <TextComponent numOfLine={1} text={item.title} title size={18} />
+          <AvatarGroup />
+          <RowComponent>
+            <Location size={18} color={appColors.text3} variant="Bold" />
+            <SpaceComponent width={8} />
             <TextComponent
-                numOfLine={1}
-                text={item.title}
-                title
-                size={18}
+              flex={1}
+              numOfLine={1}
+              text={item.locationAddress}
+              size={12}
+              color={appColors.text2}
             />
-            <AvatarGroup />
-            <RowComponent>
+          </RowComponent>
+        </>
+      ) : (
+        <>
+          <RowComponent>
+            <Image
+              source={{uri: item.photoUrl}}
+              style={{
+                width: 79,
+                height: 92,
+                borderRadius: 12,
+                resizeMode: 'cover',
+              }}
+            />
+            <SpaceComponent width={12} />
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'space-between',
+                height: '100%',
+              }}>
+              <TextComponent
+                color={appColors.primary}
+                text={`${DateTime.GetDayString(item.date)} • ${DateTime.GetTime(
+                  new Date(item.startAt),
+                )}`}
+              />
+              <TextComponent text={item.title} title size={19} numOfLine={2} />
+              <RowComponent>
                 <Location size={18} color={appColors.text3} variant="Bold" />
                 <SpaceComponent width={8} />
                 <TextComponent
-                    flex={1}
-                    numOfLine={1}
-                    text={item.location.address}
-                    size={12}
-                    color={appColors.text2}
+                  flex={1}
+                  numOfLine={1}
+                  text={item.locationAddress}
+                  size={12}
+                  color={appColors.text2}
                 />
-            </RowComponent>
-        </CardComponent>
-    );
+              </RowComponent>
+            </View>
+          </RowComponent>
+        </>
+      )}
+    </CardComponent>
+  );
 };
 
 export default EventItem
