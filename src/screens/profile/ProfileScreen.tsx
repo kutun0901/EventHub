3d786de1043b1854738153977ea8a5de1a35589a
kutun0901 <tuan.tq2433@gmail.com>
2025-02-03 14:58:03 -0800

    profile screen starter

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import ContainerComponent from '../../components/ContainerComponent'
+import { ButtonComponent } from '../../components'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { removeAuth } from '../../redux/reducers/authReducer'
 
 const ProfileScreen = () => {
+
+  const dispatch = useDispatch()
+
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+   <ContainerComponent back>
+    <Text>
+      Profile Screen
+    </Text>
+    <ButtonComponent
+    type='primary'
+    text='Logout'
+    onPress={async () => {
+      await GoogleSignin.signOut();
+
+      dispatch(removeAuth({}))
+    }}
+    />
+
+   </ContainerComponent>
   )
 }
 
